500134803ebf0280abd87d4d95308bb677f1bde1
santiagomary1004 <22620262@tlaxiaco.tecnm.mx>
2025-03-26 23:09:09 -0600

    Modificando estructura del proyecto

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,18 @@
 <script setup>
+// Importacion de HelloWorld
 import HelloWorld from './components/HelloWorld.vue'
+// Importacion de recorrido
+import recorrido from './components/recorrido.vue';
+// Importacion de recorrido1
+import recorrido1 from './components/recorrido1.vue';
 </script>
 
 <template>
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
+  <!-- <HelloWorld msg="Vite + Vue" /> -->
+   <!-- <recorrido/> -->
+    <recorrido1/>
 </template>
 
 <style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
+
 </style>
