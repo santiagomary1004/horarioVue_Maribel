a8ce090a8588827eb40d44025c8ec6760cd568c2
santiagomary1004 <22620262@tlaxiaco.tecnm.mx>
2025-03-26 23:17:26 -0600

    Agregando y eliminando elementos de la estructura

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,19 @@
 <script setup>
 // Importacion de HelloWorld
-import HelloWorld from './components/HelloWorld.vue'
+// import HelloWorld from './components/HelloWorld.vue'
 // Importacion de recorrido
-import recorrido from './components/recorrido.vue';
+// import recorrido from './components/recorrido.vue';
 // Importacion de recorrido1
-import recorrido1 from './components/recorrido1.vue';
+// import recorrido1 from './components/recorrido1.vue';
+//Importacion de horario
+import Horario from './components/Horario.vue';
 </script>
 
 <template>
   <!-- <HelloWorld msg="Vite + Vue" /> -->
    <!-- <recorrido/> -->
-    <recorrido1/>
+    <!-- <recorrido1/> -->
+     <Horario/>
 </template>
 
 <style scoped>
--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,43 +1,28 @@
 <script setup>
 import { ref } from 'vue'
 
-defineProps({
-  msg: String,
-})
+const bienvenida = ref("Bienvenidos a la seccion de condicionales")
+const nombre = ref ("Maribel")
+const apellidos = ref ("Santiago Bautista" )
+// defineProps({
+//   msg: String,
+// })
 
-const count = ref(0)
 </script>
 
 <template>
-  <h1>{{ msg }}</h1>
-
-  <div class="card">
-    <button type="button" @click="count++">count is {{ count }}</button>
-    <p>
-      Edit
-      <code>components/HelloWorld.vue</code> to test HMR
-    </p>
+  <div class="container">
+    <h2 class="text-center">{{ bienvenida }}</h2>
+    <div class="col-md-6">
+      <h6 class="">{{ nombre }}</h6>
+      <h6>{{ apellidos }}</h6>
+    </div>
   </div>
+ 
 
-  <p>
-    Check out
-    <a href="https://vuejs.org/guide/quick-start.html#local" target="_blank"
-      >create-vue</a
-    >, the official Vue + Vite starter
-  </p>
-  <p>
-    Learn more about IDE Support for Vue in the
-    <a
-      href="https://vuejs.org/guide/scaling-up/tooling.html#ide-support"
-      target="_blank"
-      >Vue Docs Scaling up Guide</a
-    >.
-  </p>
-  <p class="read-the-docs">Click on the Vite and Vue logos to learn more</p>
+
 </template>
 
 <style scoped>
-.read-the-docs {
-  color: #888;
-}
+
 </style>
